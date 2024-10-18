f9e8e0140e3b41484d34a9895dd212eb1054a919
Brianna-Chan <348835448@gapps.yrdsb.ca>
2024-10-17 22:51:58 -0400

    add array

--- Queue.java
+++ Queue.java
@@ -8,6 +8,7 @@ public class Queue<Item>
         Node next;
     }
 
+    //returns whether queue is empty based on first pointer
     public boolean isEmpty()
     {
         return first == null;
@@ -15,13 +16,20 @@ public class Queue<Item>
 
     public void enqueue(Item item)
     {
+        //store old last into variable oldLast
         Node oldLast = last;
-        last = new Node()last.item = item;
+
+        //create new node and place last in queue
+        last = new Node();
+        last.item = item;
         last.next = null;
+
+        //if queue is empty, set first pointer to new node (now first node)
         if(isEmpty())
         {
             first = last;
         }
+        //if queue is not empty, set previous last node to point to newest node
         else
         {
             oldLast.next = last;
